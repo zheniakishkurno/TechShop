7bb576459ecc8fa1e81bbc453445459fe4a2fede
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-05-26 16:40:28 +0300

    config.php

--- online-shop/config.php
+++ online-shop/config.php
@@ -1,16 +1,16 @@
 <?php
 $host = 'dpg-d0q2kleuk2gs73a63960-a.oregon-postgres.render.com';
 $dbname = 'electronics_shop';
-$user = 'electronics_shop_user'; // реальный пользователь из Render
-$password = 'zSiCB74wM7hpHtqeyUDw1ewd2TOySz6U'; // реальный пароль из Render
+$user = 'electronics_shop_user';
+$password = 'zSiCB74wM7hpHtqeyUDw1ewd2TOySz6U';
 
 $dsn = "pgsql:host=$host;dbname=$dbname;sslmode=require";
 
 try {
     $pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // выбрасывать исключения при ошибках
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // возвращать ассоциативные массивы
     ]);
 } catch (PDOException $e) {
-    die("Ошибка подключения: " . $e->getMessage());
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
 }
