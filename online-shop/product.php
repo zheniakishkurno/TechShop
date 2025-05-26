e06b648b0d279d9f4b114511fa937861f0c0ccdc
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-05-26 18:26:21 +0300

    product.php

--- online-shop/product.php
+++ online-shop/product.php
@@ -16,6 +16,8 @@ $page_title = htmlspecialchars($product['name']);
  
 require_once 'header.php';
 ?>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
