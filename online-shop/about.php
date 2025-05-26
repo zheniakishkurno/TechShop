6f820fd4282a8e461a310944e5a986253f76be63
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-05-26 18:23:07 +0300

    about.php

--- online-shop/about.php
+++ online-shop/about.php
@@ -3,6 +3,8 @@ $page_title = "О сайте";
 require_once 'header.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
