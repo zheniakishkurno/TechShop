3ea78eb6cff9a90a3d76cae9e8e3ae4713761073
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-05-26 18:23:58 +0300

    contacts.php

--- online-shop/contacts.php
+++ online-shop/contacts.php
@@ -1,4 +1,6 @@
 <?php require_once 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
