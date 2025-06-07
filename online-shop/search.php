a965800f7d8df18a52039cafd1adc2e0daaa9a21
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:19:26 +0300

    Update search.php

--- online-shop/search.php
+++ online-shop/search.php
@@ -29,7 +29,7 @@ require_once 'header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="/online-shop/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
     <link rel="stylesheet" href="css/style.css">
 </head>
