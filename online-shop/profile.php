8ff1e2616d5ab8e9d2dd4ab10874680f92d579d0
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:28:06 +0300

    Update profile.php

--- online-shop/profile.php
+++ online-shop/profile.php
@@ -86,10 +86,11 @@ function getOrderStatusBadge($status) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $page_title ?? 'Онлайн магазин' ?></title>
     
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
+    <title><?= $page_title ?? 'Онлайн магазин' ?></title>
+    
     <!-- Подключение CSS -->
     <link rel="stylesheet" href="css/profile.css">
             <link rel="stylesheet" href="css/style.css">
