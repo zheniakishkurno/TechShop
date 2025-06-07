7f5ffedcc0e888b7365aa70231b07a669ae376b3
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:02:15 +0300

    Update header.php

--- online-shop/header.php
+++ online-shop/header.php
@@ -14,7 +14,7 @@ $current_user = getCurrentUser();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($page_title) ? "$page_title | " : "" ?>TechShop</title>
 
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/favicon.ico" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="/online-shop/css/style.css">
