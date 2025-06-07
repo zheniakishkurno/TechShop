6afb8c69fdf88cc85db330273857ac1194c4dc51
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:25:28 +0300

    Update cart.php

--- online-shop/cart.php
+++ online-shop/cart.php
@@ -73,10 +73,11 @@ require_once 'header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    
     <meta name="description" content="TechShop - лучший магазин электроники с огромным выбором гаджетов по доступным ценам.">
     <title>TechShop</title>
-
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
     <link rel="stylesheet" href="css/style.css">
 </head>
