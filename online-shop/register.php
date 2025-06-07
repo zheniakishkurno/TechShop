ed0efe15205c9d5b2b42cbf5aa94891cd751a4cb
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 20:24:46 +0300

    Update register.php

--- online-shop/register.php
+++ online-shop/register.php
@@ -79,6 +79,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Регистрация | TechShop</title>
+    
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
     <link rel="stylesheet" href="css/register.css">
 </head>
 <body>
