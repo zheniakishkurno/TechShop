edf9ceb7c10ad7f7f2bd9338ca104b59159ec021
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:02:03 +0300

    Update footer.php

--- online-shop/footer.php
+++ online-shop/footer.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechShop</title>
 
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/favicon.ico" />
 
     <!-- Подключение CSS -->
     <link rel="stylesheet" href="/online-shop/css/style.css">
