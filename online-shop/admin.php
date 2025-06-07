8edbba1003c0119c998b3c509b587bbc8629e37c
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-07 21:24:23 +0300

    Update admin.php

--- online-shop/admin.php
+++ online-shop/admin.php
@@ -455,10 +455,11 @@ $reviews = $pdo->query("SELECT
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    
     <title>Админ панель</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     
     <link rel="stylesheet" href="css/admin.css" />
     <script src="js/admin.js" defer></script>
