cc50c4156a656a0f5d18111e9415651c6f93220d
zhenia <150469783+zheniakishkurno@users.noreply.github.com>
2025-06-13 14:18:55 +0300

    Update config.php

--- online-shop/config.php
+++ online-shop/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'dpg-d0q2kleuk2gs73a63960-a.oregon-postgres.render.com';
+$host = 'dpg-d15vvs7diees73eiqul0-a.oregon-postgres.render.com';
 $dbname = 'electronics_shop';
-$user = 'electronics_shop_user';
-$password = 'zSiCB74wM7hpHtqeyUDw1ewd2TOySz6U';
+$user = ' electronics_shop_d7t7_user';
+$password = 'NA3a44kFrN8KQg4H4zP8TEqdUZFeOhAc';
 
 $dsn = "pgsql:host=$host;dbname=$dbname;sslmode=require";
 
